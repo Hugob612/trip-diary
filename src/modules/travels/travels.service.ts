c0f64ae2ccb2f8cba2263574d1b2327bd41e30d4
Hugob612 <133262080+Hugob612@users.noreply.github.com>
2023-12-12 16:38:11 -0300

    1

--- src/modules/travels/travels.service.ts
+++ src/modules/travels/travels.service.ts
@@ -4,18 +4,20 @@ import { TravelDTO } from './travel.dto';
 
 @Injectable()
 export class TravelsService {
+  findAll() {
+    throw new Error('Method not implemented.');
+  }
   constructor(private prisma: PrismaService) {}
 
   async create(data: TravelDTO) {
-
     const travelExists = await this.prisma.travel.findFirst({
-        where: {
-            id: data.id
-        }
-    })
+      where: {
+        id: data.id,
+      },
+    });
 
     if (travelExists) {
-        throw new Error('Travel already exists')
+      throw new Error('Travel already exists');
     }
 
     const travel = await this.prisma.travel.create({
@@ -23,5 +25,9 @@ export class TravelsService {
     });
 
     return travel;
+
+    async findAll() {
+      return this.prisma.travel.findMany();
+    }
   }
 }
