8b6398d200341237837ff0ddb970fdecedfc41a6
Hugob612 <133262080+Hugob612@users.noreply.github.com>
2024-02-21 22:00:01 -0300

    5

--- src/modules/travels/travels.service.ts
+++ src/modules/travels/travels.service.ts
@@ -1,12 +1,44 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
 import { TravelDTO } from './travel.dto';
 import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
 export class TravelService {
-  contructor( private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async create(data: TravelDTO) {
-    this.prisma}
+    const travelExists = await this.prisma.travel.findFirst({
+      where: {
+        date: data.date,
+        destiny: data.destiny,
+      },
+    });
+    if (travelExists) {
+      throw new Error('Travel already exists');
+    }
+
+    const travel = await this.prisma.travel.create({
+      data,
+    });
+    return travel;
+  }
+  async findAll() {
+    return this.prisma.travel.findMany();
+  }
+
+  async update(id: string, data: TravelDTO) {
+    const travelExists = await this.prisma.travel.FindUnique({
+      where: { id },
+      data,
+    });
+    
+    if (!travelExists) {
+      throw new Error('Travel not found');
+    }
+
+   return await this.prisma.travel.update({
+      data,
+      where: { id },
+    });
+  }
 }
